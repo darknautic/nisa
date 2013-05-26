da06edcfd35a35a2d36e15c3e86d238e1966f62f
darknautic <blue.darknautic@gmail.com>
2013-05-26 04:04:19 -0500

    new look backgroun white and logo

--- js/main.js
+++ js/main.js
@@ -8,82 +8,84 @@ $(function(){
 	$("#serv1").click(function(){ 
 			$("#mainContainer").empty();
 			$("#mainContainer").html("<h2>&raquo; Diseño de modelos de seguridad de la información</h2> <p>Alineamos sus procesos de negocio para cumplir con estándares internacionales y nacionales, para proporcionar seguridad al activo más importante, la información: <ul><li>ISO/IEC 27001</li><li>COBIT</li><li>MAAGTICSI</li></ul></p>");
-			$(".span4").css('color','#131313');			
+			$(".span4").css('color','#ffffff');	
 	});
 
 	$("#serv2").click(function(){ 
 			$("#mainContainer").empty();
 			$("#mainContainer").html("<h2>&raquo; Apoyo en el cumplimiento de la normatividad aplicable</h2> <p>Guiamos a su negocio, con herramientas y asesoría, para cumplir con la normatividad aplicable en el tema de la seguridad de la información: <ul><li>MAAGTICSI</li><li>Normas Mexicanas</li></ul></p>");
-			$(".span4").css('color','#131313');			
+			$(".span4").css('color','#ffffff');	
 	});
 
 	$("#serv3").click(function(){ 
 			$("#mainContainer").empty();
 			$("#mainContainer").html("<h2>&raquo; Análisis de Riesgos</h2> <p>En la actualidad las Tecnologías de la Información y Comunicaciones, pueden ser blanco de amenazas que aprovechan alguna vulnerabilidad en los sistemas o activos, y de concretarse el escenario de riesgo puede llegar a tener un impacto en el negocio. Por lo cual es importante conocer los escenarios de riesgo, para gestionarlos y mitigarlos.</p>");
-			$(".span4").css('color','#131313');			
+			$(".span4").css('color','#ffffff');	
 	});
 
 
 	$("#serv4").click(function(){ 
 			$("#mainContainer").empty();
 			$("#mainContainer").html("<h2>&raquo; Análisis Forense</h2> <p>Al concretarse un incidente informático, causando un impacto económico  en el negocio o de otro tipo, es importante conocer el origen y las causas del incidente. Nosotros le proporcionamos el apoyo técnico y legal, para hallar el origen y proceder conforme a la legislación vigente.</p>");
-			$(".span4").css('color','#131313');			
+			$(".span4").css('color','#ffffff');	
 	});
 
 	$("#serv5").click(function(){ 
 			$("#mainContainer").empty();
 			$("#mainContainer").html("<h2>&raquo; Pruebas de Penetración/Hacking Ético</h2> <p>Con estas pruebas, simulamos el comportamiento de un hacker o de un usuario mal intencionado, para identificar las vulnerabilidades en los sistemas y activos de información que ellos pudieran aprovechar para concretar un incidente. El objetivo es conocer la debilidad de su negocio  y en qué grado está expuesto.</p>");
-			$(".span4").css('color','#131313');			
+			$(".span4").css('color','#ffffff');	
 	});
 
 	$("#serv6").click(function(){ 
 			$("#mainContainer").empty();
 			$("#mainContainer").html("<h2>&raquo; Equipo de Respuesta a Incidentes de Seguridad de la Información</h2> <p>Ante la creciente evolución y uso de las TIC, con lo cual también están presentes los incidentes en la seguridad de la información (tecnológicos o de gestión), es importante que las organizaciones y empresas consideren tener un Equipo de Respuesta para su atención. NISA los apoya con capacitación y consultoría para su implementación.</p>");
-			$(".span4").css('color','#131313');			
+			$(".span4").css('color','#ffffff');	
 	});
 
 	$("#serv7").click(function(){ 
 			$("#mainContainer").empty();
 			$("#mainContainer").html("<h2>&raquo; Gobierno de las TIC, para su uso eficiente</h2> <p>Lo asesoramos para alinear su infraestructura, procesos y servicios; con base en estándares internacionales y mejores prácticas, para alcanzar los objetivos de su negocio, logrando así los mejores resultados, el uso eficiente de las TIC y la mejora continúa: <ul><li>ITIL</li><li>ISO/IEC 20000</li><li>Enterprise Resource Planning (ERP)</li></ul></p>");
-			$(".span4").css('color','#131313');			
+			$(".span4").css('color','#ffffff');	
 	});
 	
 	$("#serv8").click(function(){ 
 			$("#mainContainer").empty();
 			$("#mainContainer").html("<h2>&raquo; Apoyo en el Cumplimiento de la Legislación Aplicable </h2> <p>Lo asesoramos para cumplir con las leyes aplicables en el tema de seguridad de la información, y en el desarrollo de políticas y lineamientos para proteger la información de su negocio. <ul><li>Ley Federal de Protección de Datos Personales en Posesión de los Particulares</li><li>Ley Sarbanes-Oxley</li><li>Derechos de autor</li><li>Propiedad industrial</li><li>Comercio electrónico</li><li>Derecho informático</li><li>Derecho a la información</li></ul></p>");
-			$(".span4").css('color','#131313');			
+			$(".span4").css('color','#ffffff');	
 	});
 	
 	$("#serv9").click(function(){ 
 			$("#mainContainer").empty();
 			$("#mainContainer").html("<h2>&raquo; Desarrollo de Herramientas y Aplicaciones </h2> <p>Desarrollamos aquellas herramientas y aplicaciones que su negocio necesita. <ul><li>Páginas Web</li><li>Aplicación para dispositivos móviles </li></ul></p>");
-			$(".span4").css('color','#131313');			
+			$(".span4").css('color','#ffffff');	
 	});
 
 	$("#serv10").click(function(){ 
 			$("#mainContainer").empty();
 			$("#mainContainer").html("<h2>&raquo; Análisis y Automatización de Procesos</h2> <p>Aquellos procesos que su negocio realiza manualmente o de forma semiautomática, lo apoyamos a modelarlos y automatizarlos, para de ésta forma dar valor agregado a su negocio.</p>");
-			$(".span4").css('color','#131313');			
+			$(".span4").css('color','#ffffff');			
 	});
 // $("#hideServ1").hide();
 
-	$("#contact").click(function(){ 
-			$("#mainContainer").empty();
+/*	$("#contact").click(function(){ 
+			//$("#mainContainer").empty();
 			$("#mainContainer").html("<h2><ul><li>ventas@nisa.org.mx<br></li></ul></h2>");
 			$(".span4").css('color','#131313');			
 	});
-	
-	$("#aboutNisa").click(function(){ 
-			$("#mainContainer").empty();
-			$("#mainContainer").html("<h2>Valores </h2> <p>Servimos con amor, trabajamos con honestidad y entregamos resultados de calidad. <ul><li>Amor</li><li>Servicio</li><li>Trabajo</li><li>Honestidad</li><li>Integridad</li><li>Excelencia</li><li>Responsabilidad social</li></ul></p>");
+*/	
+/*	$("#aboutNisa").click(function(){ 
+			//$("#mainContainer").empty();
+			//$("#mainContainer").html("<h2>Valores </h2> <p>Servimos con amor, trabajamos con honestidad y entregamos resultados de calidad. <ul><li>Amor</li><li>Servicio</li><li>Trabajo</li><li>Honestidad</li><li>Integridad</li><li>Excelencia</li><li>Responsabilidad social</li></ul></p>");
+			//$("#mainContainer").html("<h2>Valores </h2> <p> <ul><li>Amor</li><li>Servicio</li><li>Trabajo</li><li>Honestidad</li><li>Integridad</li><li>Excelencia</li><li>Responsabilidad social</li></ul></p>");
 			$(".span4").css('color','#B8B8B8');
 	});
+*/
 
-	$("#verMas").click(function(){ 
+	/*$("#verMas").click(function(){ 
 			$("#mainContainer").empty();
 			$("#mainContainer").html("<h2>Servicios.</h2><p><ul><li>Diseño de Modelos de Seguridad de la Información</li><li>Análisis de Riesgos</li><li>Apoyo en el Cumplimiento de la Normatividad Aplicable</li><li>Análisis Forense</li><li>Pruebas de Penetración/Hacking Ético</li><li>Equipo de Respuesta a Incidentes de Seguridad de la Información</li><li>Gobierno de las TIC, para su uso Eficiente</li><li>Análisis y Automatización de Procesos</li><li>Desarrollo de Herramientas y Aplicaciones </li><li>Apoyo en el Cumplimiento de la Legislación Aplicable</li></ul></p>");
 			$(".span4").css('color','#131313');			
 	});
-
-	$("#mainTitle").click(function(){location.reload();});
+*/
+	//$("#mainTitle").click(function(){location.reload();});
 });
